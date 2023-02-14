9507f91444c82cb90aaeacec85bf48a8d33f638f
andreapaniagua <46696984+andreapaniagua@users.noreply.github.com>
2023-02-13 23:42:08 -0600

    Add files via upload

---
+++ NR-HDT2.py
@@ -0,0 +1,45 @@
+#Andrea Paniagua
+#carne 18733
+#seccion 20
+
+import math
+from tabulate import tabulate
+from sympy import *
+
+#Ejercicio 1
+def func(a):
+    x=symbols('x')
+    F=(x**3+2524*x**2+3783)
+    f1=F.evalf(subs={x:a})
+    dx=diff(F,x)
+    f2=dx.evalf(subs={x:a})
+    f=f1/f2
+    return f
+#Función de % de error
+def error(a,b):
+    x=abs((a-b)/a)*100
+    return x
+
+#EJERCICIO 1 NEWTON-RHAPSON
+flag =True
+temp=int(input("ingrese cuantas iteraciones desea: "))
+xn=0
+x=float(input("ingrese un número x_0: "))
+er=100
+err=float(input("cual es el % de error deseado: "))
+n=0
+l=[]
+l.append([n,x,xn,er])
+while flag!=False:
+    if n==temp or err>er:
+        flag=False
+    n+=1
+    f=func(x)
+    temp=xn
+    xn=x-f
+    x=xn
+    er=error(x,temp)
+    l.append([n,x,xn,er])
+print(tabulate(l,headers=['n','x','xn','%error']))
+
+input("presione enter")
---
+++ punto Fijo-HDT2.py
@@ -0,0 +1,70 @@
+#Andrea Paniagua
+#carne 18733
+#seccion 20
+
+from tabulate import tabulate
+from sympy import *
+#Funcion % de error
+def error(a,b):
+    x=abs((a-b)/a)*100
+    return x
+#Ejercicio 1
+def func(a):
+    x=symbols('x')
+    F= (x**2 -3*x +1)
+    f1=F.evalf(subs={x:a})
+    return f1
+#Ejercicio 2
+def func2(a):
+    x=symbols('x')
+    F=asin(5/(9*exp(-x)))/(2*pi)
+    f1=F.evalf(subs={x:a})
+    return f1
+#EJERCICIO 1 PUNTO FIJO
+n=0
+er=100
+flag = True
+imax=int(input("cuantas iteraciones desea: "))
+err=float(input("ingrese el % de error deseado: "))
+temp=100
+x=float(input("ingrese un valor x_0: "))
+l=[]
+l.append([n,x,er])
+
+while flag!=False:
+    if  n>imax-2 or err>er :
+        flag=False
+    n+=1
+    temp=float(x)
+    x=float(func(temp))
+    if x !=0:
+        er=round(error(x,temp),15)
+    l.append([n,x,er])
+    
+print(tabulate(l,headers=['n','x','%error']))
+
+
+input("Presione enter")
+
+#EJERCICIO 2 PUNTO FIJO
+n=0
+er=100
+flag = True
+imax=int(input("cuantas iteraciones desea: "))
+err=float(input("ingrese el % de error deseado: "))
+temp=100
+x=float(input("ingrese un valor x_0: "))
+l2=[]
+l2.append([n,x,er])
+
+while flag!=False:
+    if  n>imax-2 or err>er :
+        flag=False
+    n+=1
+    temp=float(x)
+    x=float(func2(temp))
+    if x !=0:
+        er=round(error(x,temp),15)
+    l2.append([n,x,er])
+    
+print(tabulate(l2,headers=['n','x','%error']))
